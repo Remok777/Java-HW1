4acd867bebf98a67d0f4993a29046c276d61d0b4
Remok777 <roman.sadokha01@gmail.com>
2020-10-24 10:19:06 +0300

    Added checking statement

--- Main.java
+++ Main.java
@@ -1,8 +1,33 @@
 package com.company;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, brick");
+    public static void main(String[] args) throws IOException {
+        int A, B, C, x, y;
+        String a, b, c;
+        x = 30;
+        y = 15;
+
+        Reader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(inputStreamReader);
+
+        System.out.print("Please enter A side in cm: ");
+        a = br.readLine();
+        System.out.print("Please enter B side in cm: ");
+        b = br.readLine();
+        System.out.print("Please enter C side in cm: ");
+        c = br.readLine();
+
+        A = Integer.parseInt(a);
+        B = Integer.parseInt(a);
+        C = Integer.parseInt(a);
+
+        if ( (A <= x && B <= y) || (A <= x && C <= y) || (B <= x && A <= y)
+            || (B <= x && C <= y) || ( C <= x && A <= y) || (C <= x && B <= y)) {
+            System.out.println("Brick is in!");
+        }
+
+
     }
 }
